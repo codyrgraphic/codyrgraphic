78c3304e3293efea32d1c48aef185cf18bfd594c
codyrgraphic <codyrgraphic@gmail.com>
2023-04-26 00:39:41 +0800

    Made animation amazing - Nav dots - event handlers creating loops with target-frame being updated at the end of any function - Updated image sequence

--- scripts/image-sequence.js
+++ scripts/image-sequence.js
@@ -1,42 +1,98 @@
 const html = document.documentElement;
 const canvas = document.getElementById("image-sequence");
 const context = canvas.getContext("2d");
-
 const frameCount = 250;
+const preloadedImages = [];
+let frameIndex = 0;
+
+window.scrollTo(0, 0);
+
+canvas.width = 1080;
+canvas.height = 1080;
+
 const currentFrame = (index) =>
   `images/image-sequence-jpg/${index.toString().padStart(4, "0")}.jpg`;
 
-const preloadedImages = [];
-
 const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
+  for (let i = 0; i < frameCount; i++) {
     const img = new Image();
-    img.src = currentFrame(i);
+    img.src = currentFrame(i + 1);
     preloadedImages.push(img);
   }
 };
+
+const updateImage = (index) => {
+  const imageToDraw = preloadedImages[index];
+  if (imageToDraw) {
+    context.drawImage(imageToDraw, 0, 0);
+  }
+  console.log("Image:", imageToDraw);
+  console.log("Index:", index);
+};
+
+const updateScrollPosition = (index) => {
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const newScrollTop = (index / (frameCount - 1)) * maxScrollTop;
+  window.scrollTo(0, newScrollTop);
+};
+
+const easeOutCubic = (t) => {
+  return 1 - Math.pow(1 - t, 3);
+};
+
+const animateFrames = async (startIndex, endIndex) => {
+  const step = startIndex < endIndex ? 1 : -1;
+  const totalFrames = Math.abs(endIndex - startIndex);
+  const duration = 600; 
+
+  const startTime = performance.now();
+
+  const animate = () => {
+    const currentTime = performance.now();
+    const elapsedTime = currentTime - startTime;
+    const progress = Math.min(elapsedTime / duration, 1);
+    const easedProgress = easeOutCubic(progress);
+    const currentFrame = Math.round(
+      startIndex + step * easedProgress * totalFrames
+    );
+
+    updateImage(currentFrame);
+
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      frameIndex = endIndex;
+    }
+  };
+
+  animate();
+};
+
+const scrollToFrame = (targetFrame) => {
+  animateFrames(frameIndex, targetFrame).then(() => {
+    frameIndex = targetFrame; 
+    updateScrollPosition(frameIndex);
+  });
+};
+
+const navDots = document.querySelectorAll(".nav-dot");
+
+navDots.forEach((dot) => {
+  dot.addEventListener("click", () => {
+    const frame = dot.getAttribute("data-frame");
+    scrollToFrame(parseInt(frame));
+  });
+});
 
 preloadImages();
 
 const img = new Image();
 img.src = currentFrame(1);
-canvas.width = 1080;
-canvas.height = 1080;
 img.onload = function () {
   context.drawImage(img, 0, 0);
 };
 
-const updateImage = (index) => {
-  console.log("Updating image with index:", index); // Keep this line
-  const imageToDraw = preloadedImages[index - 1];
-  console.log("Image object:", imageToDraw); // Add this line to log the image object
-  context.drawImage(imageToDraw, 0, 0);
-};
-
-let frameIndex = 0; // Declare the variable outside the event listener
-
 window.addEventListener("scroll", () => {
-  console.log("Scroll event triggered");
   const scrollTop =
     window.pageYOffset ||
     document.documentElement.scrollTop ||
@@ -48,10 +104,8 @@ window.addEventListener("scroll", () => {
     Math.ceil(scrollFraction * frameCount)
   );
 
-  // Only update the image if the frameIndex has changed
   if (frameIndex !== newFrameIndex) {
     frameIndex = newFrameIndex;
-    console.log(frameIndex); // Log the frameIndex value
-    updateImage(frameIndex); // Call the updateImage function
+    updateImage(frameIndex);
   }
 });
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-
 (() => {
-  const menuButton = document.getElementById('menu-button');
-  const dropdownMenu = document.querySelector('.dropdown-menu');
-  const dropdownWrapper = document.querySelector('#dropdown-wrapper');
+  // DOM Elements
+  const menuButton = document.getElementById("menu-button");
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+  const dropdownWrapper = document.querySelector("#dropdown-wrapper");
   const headerEffect1 = document.querySelector(".header-effect");
   const headerEffect2 = document.querySelector(".container-header");
 
-  console.log(menuButton);
-  console.log(dropdownMenu);
 
+
+  // Functions
   function redrawElement(element) {
     const display = element.style.display;
     element.style.display = "none";
@@ -16,9 +16,28 @@
     element.style.display = display;
   }
 
-  menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('open');
-    dropdownWrapper.classList.toggle('open');
+  function isMessengerAppInstalled() {
+    if (typeof FB !== "undefined" && FB !== null) {
+      FB.getDeferredLoadState().then((result) => {
+        if (result === "READY") {
+          openMessengerInApp();
+        } else {
+          window.open("https://m.me/274598629331502", "_blank");
+        }
+      });
+    } else {
+      window.open("https://m.me/274598629331502", "_blank");
+    }
+  }
+
+  function openMessengerInApp() {
+    window.location.href = "fb-messenger://user-thread/274598629331502";
+  }
+
+  // Event Listeners
+  menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("open");
+    dropdownWrapper.classList.toggle("open");
   });
 
   window.addEventListener("resize", () => {
@@ -26,27 +45,6 @@
     redrawElement(headerEffect2);
   });
 
-  // Facbook messenger mobile checker
-  // function isMobileDevice() {
-  //   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-  // };
-  
-  function isMessengerAppInstalled() {
-    if (typeof FB !== "undefined" && FB !== null) {
-      FB.getDeferredLoadState().then(function(result) {
-        if (result === 'READY') {
-          openMessengerInApp();
-        } else {
-          window.open('https://m.me/274598629331502', '_blank');
-        }
-      });
-    } else {
-      window.open('https://m.me/274598629331502', '_blank');
-    }
-  };
-  
-  function openMessengerInApp() {
-    window.location.href = 'fb-messenger://user-thread/274598629331502';
-  };
 
-})()
+
+})();
